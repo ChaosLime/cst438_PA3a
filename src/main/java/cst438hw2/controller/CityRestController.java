d6c26c50ad265053b3cff1b6419434e0b10cb827
Chaoslime <nicholas3000@sbcglobal.net>
2020-07-05 14:28:13 -0700

    Updated REST controller, removed weather references

--- src/main/java/cst438hw2/controller/CityRestController.java
+++ src/main/java/cst438hw2/controller/CityRestController.java
@@ -1,39 +1,29 @@
 package cst438hw2.controller;
 
-import cst438hw2.domain.CityInfo;
-import cst438hw2.domain.TempAndTime;
-
-import cst438hw2.service.CityService;
-import cst438hw2.service.WeatherService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import cst438hw2.domain.CityInfo;
+import cst438hw2.service.CityService;
 
 @RestController
 public class CityRestController {
 
-	@Autowired
-	private CityService cityService;
+  @Autowired
+  private CityService cityService;
 
-	@Autowired
-	private WeatherService weatherService;
+  @GetMapping("/api/cities/{city}")
+  public ResponseEntity<CityInfo> getWeather(@PathVariable("city") String cityName) {
 
-	@GetMapping("/api/cities/{city}")
-	public ResponseEntity<CityInfo> getWeather(@PathVariable("city") String cityName) {
-
-		CityInfo city = cityService.getCityInfo(cityName);
-		if (city != null) {
-			TempAndTime currentWeather = weatherService.getTempAndTime(cityName);
-			city.setTemp(currentWeather.getFarTemp());
-			return new ResponseEntity<CityInfo>(city, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<CityInfo>(HttpStatus.NOT_FOUND);
-		}
-	}
+    CityInfo city = cityService.getCityInfo(cityName);
+    if (city != null) {
+      return new ResponseEntity<CityInfo>(city, HttpStatus.OK);
+    } else {
+      return new ResponseEntity<CityInfo>(HttpStatus.NOT_FOUND);
+    }
+  }
 
 }
