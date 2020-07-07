7de2bc05106b365de336829b91fdb411fe1df56a
Mitchell Saunders <d3thr3lic@gmail.com>
2020-07-07 15:24:55 -0700

    Removed extra whitespace between lines of code where there was an excess. Converted some tabs to two spaces. Ran all code and tests. Visually inpsected the JUnit tests that they met the requirements.

--- src/main/java/cst438hw2/domain/CityRepository.java
+++ src/main/java/cst438hw2/domain/CityRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
-	List<City> findByName(String name);
+  List<City> findByName(String name);
 }
