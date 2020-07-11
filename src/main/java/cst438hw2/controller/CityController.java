7771990506913b1dd6a908f6c5939168b696dcfd
Chaoslime <nicholas3000@sbcglobal.net>
2020-07-11 15:32:40 -0700

    Assignment PA3A written to design spec.

--- src/main/java/cst438hw2/controller/CityController.java
+++ src/main/java/cst438hw2/controller/CityController.java
@@ -1,29 +1,42 @@
 package cst438hw2.controller;
 
-import cst438hw2.domain.CityInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import cst438hw2.domain.CityInfo;
 import cst438hw2.service.CityService;
 
 @Controller
 public class CityController {
 
-	@Autowired
-	private CityService cityService;
+  @Autowired
+  private CityService cityService;
 
-	@GetMapping("/cities/{city}")
-	public String getWeather(@PathVariable("city") String cityName, Model model) {
-		CityInfo cityResults = (CityInfo) cityService.getCityInfo(cityName);
+  @GetMapping("/cities/{city}")
+  public String getWeather(@PathVariable("city") String cityName, Model model) {
+    CityInfo cityResults = (CityInfo) cityService.getCityInfo(cityName);
 
-		if (cityResults == null) {
-			model.addAttribute("name", cityName);
-			return "city_not_found";
-		} else {
-			model.addAttribute("cityInfo", cityResults);
-			return "city";
-		}
-	}
+    if (cityResults == null) {
+      model.addAttribute("name", cityName);
+      return "city_not_found";
+    } else {
+      model.addAttribute("cityInfo", cityResults);
+      return "city";
+    }
+  }
+
+  @PostMapping("/cities/reservation")
+  public String createReservation(@RequestParam("name") String cityName,
+      @RequestParam("level") String level, @RequestParam("email") String email, Model model) {
+
+    model.addAttribute("city", cityName);
+    model.addAttribute("level", level);
+    model.addAttribute("email", email);
+    cityService.requestReservation(cityName, level, email);
+    return "request_reservation";
+  }
 }
--- src/main/java/cst438hw2/service/CityService.java
+++ src/main/java/cst438hw2/service/CityService.java
@@ -1,6 +1,8 @@
 package cst438hw2.service;
 
 import java.util.List;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import cst438hw2.domain.City;
@@ -22,6 +24,12 @@ public class CityService {
   @Autowired
   private WeatherService weatherService;
 
+  @Autowired
+  private RabbitTemplate rabbitTemplate;
+
+  @Autowired
+  private FanoutExchange fanout;
+
   public CityInfo getCityInfo(String cityName) {
 
     List<City> city = cityRepository.findByName(cityName);
@@ -36,6 +44,14 @@ public class CityService {
     }
 
     return null;
+  }
+
+  public void requestReservation(String cityName, String level, String email) {
+    String msg = "{\"cityName\": \"" + cityName + "\" \"level\": \"" + level + "\" \"email\": \""
+        + email + "\"}";
+    System.out.println("Sending message:" + msg);
+    rabbitTemplate.convertSendAndReceive(fanout.getName(), "", // routing key none.
+        msg);
   }
 
 }
